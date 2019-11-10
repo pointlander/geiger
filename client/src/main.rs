b8276c8cff9fe7f0c3a5a487750f7ea0fd6696f5
Andrew Snodgrass <git@pointlander.info>
2019-11-10 23:50:38 +0000

    Added geiger i2c start and stop

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,34 @@
 use std::error::Error;
+use std::fmt;
 
 use crossbeam_channel::{bounded, Receiver, select};
 use ctrlc;
 use rppal::gpio::{Gpio, Trigger};
+use rppal::i2c::I2c;
 use rppal::system::DeviceInfo;
+
+#[derive(Debug)]
+struct MyError {
+    details: String
+}
+
+impl MyError {
+    fn new(msg: &str) -> MyError {
+        MyError{details: msg.to_string()}
+    }
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
+impl Error for MyError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
 
 fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
 	let (sender, receiver) = bounded(100);
@@ -15,11 +40,21 @@ fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
 }
 
 const GPIO_GEIGER: u8 = 4;
+const I2C_ADDRESS: u16 = 0x19;
+const I2C_START: [u8; 1] = [0x71];
+const I2C_STOP: [u8; 1] = [0x00];
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let ctrl_c_events = ctrl_channel()?;
 
 	println!("Starting up geiger on a {}.", DeviceInfo::new()?.model());
+	let mut i2c = I2c::new()?;
+	i2c.set_slave_address(I2C_ADDRESS)?;
+	println!("sending geiger start");
+	let written = i2c.write(&I2C_START)?;
+	if written != 1 {
+		return Err(Box::new(MyError::new("start not written")));
+	}
 
 	let gpio = Gpio::new()?;
 
@@ -28,13 +63,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 		counter = counter + 1;
 		println!("particle {} {}", level, counter);
 	};
-	let mut pin = gpio.get(GPIO_GEIGER)?.into_input_pulldown();
+	let mut pin = gpio.get(GPIO_GEIGER)?.into_input();
 	pin.set_async_interrupt(Trigger::FallingEdge, particle)?;
 
 	loop {
 		select! {
 			recv(ctrl_c_events) -> _ => {
-				println!("Exiting...");
+				println!("sending geiger stop");
+				let written = i2c.write(&I2C_STOP)?;
+				if written != 1 {
+					return Err(Box::new(MyError::new("stop not written")));
+				}
                 		break;
             		}
         	}
